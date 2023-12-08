ae495c5aa47330c01d945eaed07348c423b292ff
Olivia Brotherus <olivia@brotherus.fi>
2023-12-08 21:17:55 +0200

    fixes

--- viikko6/query-language/src/query_builder.py
+++ viikko6/query-language/src/query_builder.py
@@ -3,24 +3,33 @@ from matchers import *
 class QueryBuilder:
     def __init__(self):
         self.query = All()
+        self.matchers = []
+        self.or_query = False
 
     def build(self):
+        if self.or_query:
+            self.query = Or(*self.matchers)
+        else:
+            self.query = All()
+            for matcher in self.matchers:
+                self.query = And(self.query, matcher)
+        
         return self.query
-    
+
     def playsIn(self, team):
-        self.query = And(self.query, PlaysIn(team))
+        self.matchers.append(PlaysIn(team))
         return self
     
     def hasAtLeast(self, value, attr):
-        self.query = And(self.query, HasAtLeast(value, attr))
+        self.matchers.append(HasAtLeast(value, attr))
         return self
     
     def hasFewerThan(self, value, attr):
-        self.query = And(self.query, HasFewerThan(value, attr))
+        self.matchers.append(HasFewerThan(value, attr))
         return self
     
     def oneOf(self, *matchers):
-        for matcher in matchers:
-            self.query = Or(self.query, matcher)
+        self.or_query = True
+        self.matchers = matchers
         return self
         
